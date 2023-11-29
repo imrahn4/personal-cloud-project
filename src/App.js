fee0699d248f730fa414ecc31bbda8e854b89204
Potato <imrahnfaqiri21@gmail.com>
2023-11-29 01:30:12 -0500

    basic sidebar with animations

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import { IoSettingsOutline } from "react-icons/io5";
+import { RxDashboard } from "react-icons/rx";
+import { FaRegHeart } from "react-icons/fa";
+import { BsTags } from "react-icons/bs";
+import { MdOutlineFileUpload } from "react-icons/md";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="app-container">
+      
+      {/* Sidebar */}
+      <div className="sidebar"> 
+        <div className="sidebar-item">
+          <RxDashboard className="sidebar-item-icon"/>
+          <p className="sidebar-item-text"> All </p>
+        </div>
+        <div className="sidebar-item">
+          <FaRegHeart className="sidebar-item-icon"/>
+          <p className="sidebar-item-text"> Favourites </p>
+        </div>
+        <div className="sidebar-item">
+          <BsTags className="sidebar-item-icon"/>
+          <p className="sidebar-item-text"> Tags </p>
+        </div>
+        <div className="sidebar-item">
+          <MdOutlineFileUpload className="sidebar-item-icon"/>
+          <p className="sidebar-item-text"> Upload </p>
+        </div>
+
+        <div className="sidebar-item-upload">
+          <IoSettingsOutline className="sidebar-item-icon"/>
+          <p className="sidebar-item-text"> Settings </p>
+        </div>
+      </div>
+      
+      {/* Main Content */}
+      <div className="main-content"> 
+        <> Content </>
+      </div>
+
     </div>
+
   );
 }
 
